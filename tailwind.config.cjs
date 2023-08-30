12939dbb907c48f3742c687376b892c03df57683
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-08-30 11:16:43 -0400

    Initialize astro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,8 @@
-/** @type {import('tailwindcss').Config}*/
-const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {}
+		extend: {},
 	},
-
-	plugins: []
-};
-
-module.exports = config;
+	plugins: [],
+}
